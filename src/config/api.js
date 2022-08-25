4af7ff57642a65db38e0d56bd13b6e921f379190
DIAH FN <diahfn26@gmail.com>
2022-08-25 15:45:57 +0700

    first commit

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,5 @@
 export const API = () => {
-    const baseUrl = process.env.REACT_APP_SERVER_URL ||
-                    'https://cinema-b36.herokuapp.com/api/v1' ||
+    const baseUrl = 'https://cinema-b36.herokuapp.com/api/v1' ||
                     'http://localhost:5000/api/v1'
 
     const executeAPI = async (endpoint, config) => {
