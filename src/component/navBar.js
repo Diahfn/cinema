7f317befdf6826cbaccd29837e3714e727f08911
DIAH FN <diahfn26@gmail.com>
2022-08-25 15:13:34 +0700

    first commit

--- src/component/navBar.js
+++ src/component/navBar.js
@@ -12,7 +12,7 @@ import brand from '../assets/brand.png'
 import { UserContext } from '../context/userContext'
 import Login from './auth/login'
 import Register from './auth/register'
-
+ 
 export default function NavBar() {
 
     const navigate = useNavigate()
@@ -46,63 +46,78 @@ export default function NavBar() {
     <>
         <Navbar expand='lg' className='sticky-top mx-0' style={{background: '#0D0D0D'}}>
             <Container>
-                <Navbar.Brand href='/homepage'>
-                    <img
-                        alt='brand'
-                        src={brand}
-                    />
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls='basic-navbar-nav' />
-                <Navbar.Collapse id='responsive-navbar-nav'>
                     
-                    {!state.isLogin ? (
+                {!state.isLogin ? (
+                    <>
+                       <Navbar.Brand href='/auth'>
+                            <img
+                                alt='brand'
+                                src={brand}
+                            />
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                        <Navbar.Collapse id='responsive-navbar-nav'>
+
                         <Nav className='ms-auto py-0'>
-                        <Nav.Link className='auth mx-3' onClick={handleLogin}>Login</Nav.Link>
-                        <Nav.Link className='auth mx-3' style={{backgroundColor: '#CD2E71'}} onClick={handleRegister}>Register</Nav.Link>
-                        
-                    </Nav>
-                    ) : (
+                            <Nav.Link className='auth mx-3' onClick={handleLogin}>Login</Nav.Link>
+                            <Nav.Link className='auth mx-3' style={{backgroundColor: '#CD2E71'}} onClick={handleRegister}>Register</Nav.Link>
+                            
+                        </Nav>
+                        </Navbar.Collapse>
+                    </>
+                ) : (
+                    <>
+                    <Navbar.Brand href='/homepage'>
+                        <img
+                            alt='brand'
+                            src={brand}
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                    <Navbar.Collapse id='responsive-navbar-nav'>
                         <Nav className='ms-auto py-0'>
-                        <NavDropdown 
-                            title={
-                                <div>
-                                    <img style={{width: '50px'}}
-                                        alt='user'
-                                        src={user} className='user dropdown-toggle'
+                            <NavDropdown 
+                                title={
+                                    <div>
+                                        <img style={{width: '50px'}}
+                                            alt='user'
+                                            src={user} className='user dropdown-toggle'
+                                        />
+                                    </div>
+                                }
+                                id='collasible-nav-dropdown' className='my-0 py-0 ' menuVariant='dark'
+                            >
+                                <NavDropdown.Item as={Link} to='/profile' style={{fontWeight: '600'}}>
+                                    <img
+                                        alt='profile' src={profile} className='nav-icon my-1'
                                     />
-                                </div>
-                            }
-                            id='collasible-nav-dropdown' className='my-0 py-0 ' menuVariant='dark'
-                        >
-                            <NavDropdown.Item as={Link} to='/profile' style={{fontWeight: '600'}}>
-                                <img
-                                    alt='profile' src={profile} className='nav-icon my-1'
-                                />
-                                Profile
-                            </NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to='/my-film' style={{fontWeight: '600'}}>
-                                <img
-                                    alt='list' src={clip} className='nav-icon my-1'
-                                />
-                                My List Film
-                            </NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to='/complain' style={{fontWeight: '600'}}>
-                                <img
-                                    alt='' src={complain} className='nav-icon my-1 logo' 
-                                />
-                                Complain
-                            </NavDropdown.Item>
-                            <NavDropdown.Divider style={{borderColor: '#fff'}} />
-                            <NavDropdown.Item onClick={logout} style={{fontWeight: '600'}}>
-                                <img
-                                    alt='logout' src={log} className='nav-icon my-1 '
-                                />
-                                Logout
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                    )}
-                </Navbar.Collapse>
+                                    Profile
+                                </NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to='/my-film' style={{fontWeight: '600'}}>
+                                    <img
+                                        alt='list' src={clip} className='nav-icon my-1'
+                                    />
+                                    My List Film
+                                </NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to='/complain' style={{fontWeight: '600'}}>
+                                    <img
+                                        alt='' src={complain} className='nav-icon my-1 logo' 
+                                    />
+                                    Complain
+                                </NavDropdown.Item>
+                                <NavDropdown.Divider style={{borderColor: '#fff'}} />
+                                <NavDropdown.Item onClick={logout} style={{fontWeight: '600'}}>
+                                    <img
+                                        alt='logout' src={log} className='nav-icon my-1 '
+                                    />
+                                    Logout
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                    </>        
+                )}
+                
             </Container>
             <Login
             login={login}
